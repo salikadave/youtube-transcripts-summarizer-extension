fd8d0165a222adcb39c61a651172025af8f22cd5
Salika Dave <salikadave26@gmail.com>
2021-05-05 23:43:34 +0530

    fix logic for displaying and hiding progress bar & btn

--- popup.js
+++ popup.js
@@ -1,9 +1,17 @@
+let summaryCTA = document.getElementById("ctaGenerateSummary");
+let progressContainer = document.getElementById("progressHolder");
+
 document.addEventListener("DOMContentLoaded", (e) => {
-  let summaryCTA = document.getElementById("ctaGenerateSummary");
   summaryCTA.addEventListener("click", generateSummary);
 });
 
 const generateSummary = () => {
+  // disable summary button
+  summaryCTA.classList.add("disabled");
+  // show progress bar
+  progressContainer.innerHTML = `<div class="progress">
+  <div class="indeterminate"></div>
+  </div>`;
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {
       action: "generate",
@@ -15,7 +23,9 @@ const generateSummary = () => {
 chrome.runtime.onMessage.addListener(function (message) {
   if (message.action === "result") {
     console.log("printing output summary");
-    printOutputSummary(message.summaryText)
+    summaryCTA.classList.remove("disabled");
+    progressContainer.innerHTML = "";
+    printOutputSummary(message.summaryText);
     // window.open()
   }
 });
